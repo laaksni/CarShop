fa7b1eca30cb6494b190b12c4aa0635fc9d0e60a
laaksni <niko.laaksonen@hotmail.com>
2024-04-23 01:00:31 +0300

    Muokattu toiminallisuuksia

--- src/components/Carlist.jsx
+++ src/components/Carlist.jsx
@@ -5,7 +5,6 @@ import "ag-grid-community/styles/ag-theme-material.css";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
-//import Typography from "@mui/material/Typography";
 import Snackbar from "@mui/material/Snackbar";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -18,11 +17,20 @@ export default function Carlist() {
     const [openDialog, setOpenDialog] = useState(false);
     const [openSnackbar, setOpenSnackbar] = useState(false);
     const [snackbarMessage, setSnackbarMessage] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredCars, setFilteredCars] = useState([]);
     const gridRef = useRef();
 
     useEffect(() => {
         fetchData();
     }, []);
+
+    useEffect(() => {
+        const filteredCars = cars.filter((car) =>
+            car.brand.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredCars(filteredCars);
+    }, [cars, searchTerm]);
 
     const fetchData = () => {
         fetch("https://carrestservice-carshop.rahtiapp.fi/cars")
@@ -30,29 +38,93 @@ export default function Carlist() {
             .then((data) => setCars(data._embedded.cars));
     };
 
-    const handleFieldChange = (event) => {
-        setSelectedCar(event.data);
+    const handleFieldChange = (field, value) => {
+        setSelectedCar({
+            ...selectedCar,
+            [field]: value
+        });
+    };
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
     };
 
     const handleAddCar = () => {
-        // TODO: Implementoi uuden auton lisääminen API:lle
-        setOpenDialog(false);
-        setSnackbarMessage("Auto lisätty onnistuneesti!");
-        setOpenSnackbar(true);
+
+        const newCar = {
+            brand: selectedCar.brand,
+            model: selectedCar.model,
+            color: selectedCar.color,
+            fuel: selectedCar.fuel,
+            modelYear: selectedCar.year,
+            price: selectedCar.price
+        };
+
+        fetch("https://carrestservice-carshop.rahtiapp.fi/cars", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newCar)
+        })
+            .then(response => {
+                if (response.ok)
+                    fetchData();
+                    setSnackbarMessage("Auto lisätty onnistuneesti!");
+                    setOpenSnackbar(true);
+                    setOpenDialog(false);
+                })
+            .catch((error) => console.error("Virhe lisättäessä autoa:", error));
     };
 
     const handleDeleteCar = () => {
-        // TODO: Implementoi valitun auton poistaminen API:lle
-        setSelectedCar(null);
-        setSnackbarMessage("Auto poistettu onnistuneesti!");
-        setOpenSnackbar(true);
+        if (!selectedCar) {
+            return;
+        }
+
+        const carId = selectedCar.id;
+
+        fetch(`https://carrestservice-carshop.rahtiapp.fi/cars/${carId}`, {
+            method: "DELETE"
+        })
+            .then(() => {
+                fetchData();
+                setSelectedCar(null);
+                setSnackbarMessage("Auto poistettu onnistuneesti!");
+                setOpenSnackbar(true);
+            })
+            .catch((error) => console.error("Virhe poistettaessa autoa:", error));
     };
 
     const handleEditCar = () => {
-        // TODO: Implementoi valitun auton muokkaaminen API:lle
-        setOpenDialog(false);
-        setSnackbarMessage("Auto muokattu onnistuneesti!");
-        setOpenSnackbar(true);
+        if (!selectedCar) {
+            return;
+        }
+
+        const carId = selectedCar.id;
+        const updatedCar = {
+            brand: selectedCar.brand,
+            model: selectedCar.model,
+            color: selectedCar.color,
+            fuel: selectedCar.fuel,
+            modelYear: selectedCar.year,
+            price: selectedCar.price
+        };
+
+        fetch(`https://carrestservice-carshop.rahtiapp.fi/cars/${carId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedCar)
+        })
+            .then(() => {
+                fetchData();
+                setSnackbarMessage("Auto muokattu onnistuneesti!");
+                setOpenSnackbar(true);
+                setOpenDialog(false);
+            })
+            .catch((error) => console.error("Virhe muokattaessa autoa:", error));
     };
 
     const handleDialogClose = () => {
@@ -75,30 +147,26 @@ export default function Carlist() {
     return (
         <>
             <Stack mt={2} direction="row" spacing={2} justifyContent="center" alignItems="center">
-                {/* Hae- ja suodatuskentät */}
-                {/* TODO: Implementoi hakukenttä ja suodatustoiminnot */}
-                <TextField placeholder="Hae" />
+                <TextField placeholder="Hae"
+                    value={searchTerm}
+                    onChange={handleSearchChange} />
 
-                {/* Lisää auto -painike */}
                 <Button variant="outlined" onClick={() => setOpenDialog(true)}>
                     Lisää auto
                 </Button>
 
-                {/* Muokkaa auto -painike */}
                 <Button variant="outlined" disabled={!selectedCar} onClick={handleEditCar}>
                     Muokkaa autoa
                 </Button>
 
-                {/* Poista auto -painike */}
                 <Button variant="outlined" disabled={!selectedCar} onClick={handleDeleteCar}>
                     Poista auto
                 </Button>
             </Stack>
 
-            {/* Autotaulukko */}
             <div className="ag-theme-material" style={{ height: "500px", width: "185%", margin: "auto" }}>
                 <AgGridReact
-                    rowData={cars}
+                    rowData={filteredCars}
                     columnDefs={columnDefs}
                     onGridReady={(params) => (gridRef.current = params.api)}
                     rowSelection="single"
@@ -109,7 +177,6 @@ export default function Carlist() {
                 />
             </div>
 
-            {/* Lisää, muokkaa ja poista auto -dialogi */}
             <Dialog open={openDialog} onClose={handleDialogClose}>
                 <DialogTitle>{selectedCar ? "Muokkaa autoa" : "Lisää auto"}</DialogTitle>
                 <DialogContent>
@@ -184,7 +251,6 @@ export default function Carlist() {
                 </DialogActions>
             </Dialog>
 
-            {/* Snackbar onnistumisviesteille */}
             <Snackbar
                 open={openSnackbar}
                 autoHideDuration={3000}
